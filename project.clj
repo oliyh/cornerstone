df6114bd770116d1a37e5e7f3d60246936c7858f
oliyh <git@oliy.co.uk>
2013-08-28 10:48:54 -0700

    updating project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cornerstone "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A re-usable foundation for Clojure projects containing defaults management."
+  :url "https://github.com/oliyh/cornerstone"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
