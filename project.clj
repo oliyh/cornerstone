6134372a31e8833a5e8bdb122119d5c1969db7e7
oliyh <git@oliy.co.uk>
2013-09-15 14:17:06 -0700

    bumping minor version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cornerstone "0.1.0-SNAPSHOT"
+(defproject cornerstone "0.1.1-SNAPSHOT"
   :description "A re-usable foundation for Clojure projects containing defaults management."
   :url "https://github.com/oliyh/cornerstone"
   :license {:name "Eclipse Public License"
